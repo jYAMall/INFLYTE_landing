2aea1a106a34d7ad8ee911ac6db22e61d0e15620
Leonid Kuznetsov <gitmrche@gmail.com>
2023-01-22 14:04:20 +0200

    feat: add svg support

--- app/brand/page.jsx
+++ app/brand/page.jsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import Icon3M from '../../assets/icons/3m.svg';
 
 const BrandPage = () => (
   <div>
     <h1 className="text-black-50 font-bold text-2xl">Brand Page</h1>
     <Link href="/">Back to Home</Link>
+    <Icon3M width={24} height={24} />
   </div>
 );
 
